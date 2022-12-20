8e911a740346c313a0832ec7424d24f6abdd2bd9
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-20 10:35:35 -0600

    Section 3: Starter Code

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -4,8 +4,11 @@ import Todo from "./components/todo";
 
 function App() {
     return <div>
-        <h1>My Todos</h1>
-        <Todo text='Learn React' />
+       <Switch>
+           <Route path={'/'}>
+               <TestTemplate />
+           </Route>
+       </Switch>
     </div>;
 } //App function
 
