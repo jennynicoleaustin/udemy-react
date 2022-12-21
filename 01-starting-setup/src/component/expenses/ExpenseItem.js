1f87fbc9ac21f72ea74074e4e2fcf5fceb269dfd
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-21 12:25:06 -0600

    Section 4: - VIDEO 8 State...

--- 01-starting-setup/src/component/expenses/ExpenseItem.js
+++ 01-starting-setup/src/component/expenses/ExpenseItem.js
@@ -6,17 +6,20 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) => {
     // Must have only one root element (div) within the return statement.
     // If you only need a simple div you can wrap it in an empty carrot situation
+
+    let title = props.title;
+
     const clickHandler = () => {
-        console.log("CLICKED")
+        title = "Updated"
     }
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>CLICK ME </button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
