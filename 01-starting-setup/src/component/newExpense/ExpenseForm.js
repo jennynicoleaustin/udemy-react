88d2a69b4875200959070c6c0fb716e9edef73de
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-22 08:57:47 -0600

    Section 4: - Video 56 --> Use individual state approach then the onSubmit function handles the collection of data into a form.

--- 01-starting-setup/src/component/newExpense/ExpenseForm.js
+++ 01-starting-setup/src/component/newExpense/ExpenseForm.js
@@ -1,8 +1,8 @@
 import "./ExpenseForm.css";
-import { useState } from 'react'
+import {useState} from 'react'
 
-function ExpenseForm () {
-//  You can repeat it three separate times for state or... useState as an object as its initial value. The better more concise approach is three separate states
+function ExpenseForm() {
+    //  You can repeat it three separate times for state or... useState as an object as its initial value. The better more concise approach is three separate states
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -22,21 +22,35 @@ function ExpenseForm () {
     const dateChangeHandler = (e) => {
         setEnteredDate(e.target.value)
     }
+    const submitHandler = (e) => {
+        e.preventDefault(); // keeps the current page from reloading
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        console.log(expenseData)
+
+    }
     return (
-        <form>
+        // Add the onsubmit event to the overall form so that the submit button will trigger the event inside "onSubmit"
+        //    Don't forget that the default form behavior is to refresh the page (send the form to the server) -- in react we want to prevent default behavior so that we can handle the event ourselves, on the current page via js
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
-            </div>
                 <div className="new-expense__control">
-                <label>Amount</label>
-                <input type='number' min="0.01" max="0.01" onChange={amountChangeHandler} />
-            </div>
+                    <label>Title</label>
+                    <input type='text' onChange={titleChangeHandler}/>
+                </div>
                 <div className="new-expense__control">
-                <label>Date</label>
-                <input type='date' min="2020-01-01" max="2022-12-31" onChange={dateChangeHandler} />
-            </div>
+                    <label>Amount</label>
+                    <input type='number' min="0.01" max="0.01" onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type='date' min="2020-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                </div>
             </div>
             <div className='new-expense__actions'>
                 <button className="new-expense__control">Submit</button>
@@ -47,4 +61,7 @@ function ExpenseForm () {
 
 export default ExpenseForm;
 // If you are updating state, and it depends on other factors of state do NOT use the spread operator
-// instead use an arrow function and pass the previous state as the paramater
+// instead use an arrow function and pass the previous state as the parameter
+
+//Gather all the different states and send them via a form
+//
