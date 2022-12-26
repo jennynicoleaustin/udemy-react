c9fa168c6287e8bc19417fa6c09522623155e1a5
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-26 12:29:06 -0600

    Section 4: - V-60: Controlled vs Uncontrolled --> stateless/presentation/dumb components

--- 01-starting-setup/src/component/expenses/ExpenseItem.js
+++ 01-starting-setup/src/component/expenses/ExpenseItem.js
@@ -19,10 +19,10 @@ const ExpenseItem = (props) => {
 // Expects a default state value - useState is a variable that allows it to be called again (re-rendered) when the state changes. useState returns an array with two elements, the variable and a function for updating the function. --- We use Array destructuring to assign these values to two separate variables.
     const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle("Updated!")
-        console.log(title) // This will return the old value of title because setTitle does not actually reset the title but rather it schedules the state update.
-    }
+    // const clickHandler = () => {
+    //     setTitle("Updated!")
+    //     // console.log(title) // This will return the old value of title because setTitle does not actually reset the title but rather it schedules the state update.
+    // }
 
     // Any values that may change within the app should be set using STATE.
     //Why can you update the variable that is set as a constant - because we do not use an equal operator rather the state is updated via a function.
@@ -33,12 +33,14 @@ const ExpenseItem = (props) => {
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
+            {/*<button onClick={clickHandler}>Change Title</button>*/}
         </Card>
     );
 }
 
 export default ExpenseItem;
+
+//Stateless component (it does not manage any state) just there for presentational purposes.
