edeef3ad61a4f9a7791d863d9e35393b56a602ec
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-27 10:49:17 -0600

    Section 6: Video 74 --> Conditional styling based on isValid using inLine style

--- Section 6 /src/components/CourseGoals/CourseInput/CourseInput.js
+++ Section 6 /src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,6 +5,7 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -12,14 +13,19 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // trim will remove all the empty blank spaces from entry (essentially checking to see if length is 0 after using trim will allow us to check if the user entered nothing.)
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{borderColor: !isValid ? 'red' : 'black'}} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
