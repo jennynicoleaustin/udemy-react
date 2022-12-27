fd6be33b2eaa20c4901c150cb0f39b027a5e1e8a
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-27 11:37:40 -0600

    Section 6: Video 77 --Dynamic & Styled Components --> create your own props inside the ``

--- Section 6 /src/components/CourseGoals/CourseInput/CourseInput.js
+++ Section 6 /src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,73 +1,64 @@
 import React, { useState } from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
+
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
-&label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')};
+  }
 
-&input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
 
-&input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-
-
-&.invalid input{
-  border-color: red;
-  background: #fad0ec;
-}
-
-&.invalid label {
-  color:red;
-}
-`
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsValid] = useState(true)
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-    if(event.target.value.trim().length >0) {
-      setIsValid(true)
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    // trim will remove all the empty blank spaces from entry (essentially checking to see if length is 0 after using trim will allow us to check if the user entered nothing.)
     if (enteredValue.trim().length === 0) {
-      setIsValid(false)
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-
-      <FormControl className={!isValid ? 'invalid' : ''}>
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
-      <Button type="submit">Add Goal</Button>
-    </form>
+      <form onSubmit={formSubmitHandler}>
+        <FormControl invalid={!isValid}>
+          <label>Course Goal</label>
+          <input type="text" onChange={goalInputChangeHandler} />
+        </FormControl>
+        <Button type="submit">Add Goal</Button>
+      </form>
   );
 };
 
