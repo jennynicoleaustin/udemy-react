c54b84e17748979857d1ae7eb203a5a3ab939a0c
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-28 16:15:04 -0600

    Section 8 Error Handling modal created.

--- S8 Practice Project/src/components/user/AddUser.js
+++ S8 Practice Project/src/components/user/AddUser.js
@@ -2,11 +2,13 @@ import styles from './AddUser.module.css'
 import Card from "../ui/Card";
 import Button from "../ui/Button";
 import {useState} from "react";
+import ErrorModal from "../ui/ErrorModal";
 
 const AddUser = props => {
 
     const [enteredUsername, setEnteredUsername] = useState('')
     const [enteredAge, setEnteredAge] = useState('')
+    const [error, setError] = useState()
 
     const usernameChangeHandler = (e) => {
         setEnteredUsername(e.target.value)
@@ -19,20 +21,31 @@ const AddUser = props => {
     const addUserHandler = (e) => {
         e.preventDefault()
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: "Invalid Input",
+                message: "Please enter a valid name and age"
+            })
             return;
         }
         // + in front of the variable ensures it is a number.
         if (+enteredAge < 1) {
+            setError({
+                title: "Invalid Age",
+                message: "Please enter an age greater than 1"
+            })
             return;
         }
         setEnteredUsername("")
         setEnteredAge("")
         props.onAddUser(enteredUsername, enteredAge);
-
     }
 
+    const errorHandler = () => {
+        setError(null)
+    }
     return (
         <>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label  htmlFor="username">Username</label>
