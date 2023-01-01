279d3730ddfa6fd35d0f3cbb14743cebe3d7ffbb
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-01 17:00:24 -0600

    Section 10: Video 122 --> useContext hook

--- S10 useReducer/src/components/MainHeader/Navigation.js
+++ S10 useReducer/src/components/MainHeader/Navigation.js
@@ -1,38 +1,33 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
-  return (
-      <AuthContext.Consumer>
-          {(ctx) => {
-              return (
-                  <nav className={classes.nav}>
-                      <ul>
-                          {ctx.isLoggedIn && (
-                              <li>
-                                  <a href="/">Users</a>
-                              </li>
-                          )}
-                          {ctx.isLoggedIn && (
-                              <li>
-                                  <a href="/">Admin</a>
-                              </li>
-                          )}
-                          {ctx.isLoggedIn && (
-                              <li>
-                                  <button onClick={props.onLogout}>Logout</button>
-                              </li>
-                          )}
-                      </ul>
-                  </nav>
-              )
-          }}
+    //Call useContext and point to the context you would like to use in the app.
+    const ctx = useContext(AuthContext);
+    return (
+        <nav className={classes.nav}>
+            <ul>
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Admin</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <button onClick={props.onLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
 
-      </AuthContext.Consumer>
-
-  );
 };
 
 export default Navigation;
