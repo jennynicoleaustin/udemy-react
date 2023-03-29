64147709fb562ec28ae34c89256075a9c94c6045
Jenny Austin <jennynicoleaustin@gmail.com>
2023-03-29 13:32:15 -0500

    Section 12: V. 152

--- S12 Behind the Scences and Optimization/src/App.js
+++ S12 Behind the Scences and Optimization/src/App.js
@@ -1,14 +1,25 @@
 import React, {useState} from 'react';
 
 import './App.css';
+import Button from "./components/UI/Button/Button";
 
 function App() {
 
-    const [] = useState();
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    // real dom elements will flash when changes are made to it.
+    // You can use this to see that ONLY the elements that are effected are updated / re-rendered on the real dom.
+    console.log('app running')
+
+    const toggleParaHandler = () => {
+        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
+        {showParagraph && <p>This is the new Paragraph</p>}
+        <Button onClick={toggleParaHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
