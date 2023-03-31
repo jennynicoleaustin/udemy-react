7c5fc0872f730c0d6627c5d6ef2b7df5ef216f3e
Jenny Austin <jennynicoleaustin@gmail.com>
2023-03-31 15:41:37 -0500

    Section 15: Building Custom React Hooks -> V. 189 Configuring Custom Hooks

--- S15 Building Custom React Hooks/src/hooks/use-counter.js
+++ S15 Building Custom React Hooks/src/hooks/use-counter.js
@@ -1,15 +1,22 @@
 import {useEffect, useState} from "react";
 
-const useCounter = () => {
+const useCounter = (forward = true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1);
+            if (forward) {
+                setCounter((prevCounter) => prevCounter + 1);
+
+            } else {
+                setCounter((prevCounter) => prevCounter - 1);
+
+            }
+
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [forward]);
     // returns the number that is
     return counter;
 };
