8ed10ce491eea112a6d8257c8022a58346db8ee1
Jenny Austin <jennynicoleaustin@gmail.com>
2023-04-03 16:04:37 -0500

    Section 23: Next.js -> V. 344 Linking Between Pages

--- S23 Next JS/next-js/pages/news/index.js
+++ S23 Next JS/next-js/pages/news/index.js
@@ -1,4 +1,18 @@
-function NewsPage () {
-    return <h1>This is the News Page </h1>
+import Link from "next/link";
+
+function NewsPage() {
+    return (
+        <>
+            <h1>This is the News Page </h1>
+            <ul>
+                <li>
+                    <Link href='/news/nextjs-for-the-win'>
+                        NextJS for the win
+                    </Link>
+                </li>
+            </ul>
+        </>
+    )
 }
+
 export default NewsPage;
