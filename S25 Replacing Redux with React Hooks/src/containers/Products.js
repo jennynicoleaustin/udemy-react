92b9ec3ce3fce9da87077af8a9877eda89f1143c
Jenny Austin <jennynicoleaustin@gmail.com>
2023-04-10 14:57:44 -0500

    Section 25: Replacing Redux with React Hooks -> V.394 Custom Hook Store Summary

--- S25 Replacing Redux with React Hooks/src/containers/Products.js
+++ S25 Replacing Redux with React Hooks/src/containers/Products.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
 import { useStore } from '../hooks-store/store'
